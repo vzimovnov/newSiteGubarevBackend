1b9ab898870222a58dec56f98049da6c12d01c37
Pavel Gubarev <p.gubarev@nv.dunice.net>
2022-12-08 16:27:36 +0300

    models and migrations

---
+++ migrations/20221206083101-create-user.js
@@ -0,0 +1,47 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('User', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      login: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: { notEmpty: true },
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: { notEmpty: true },
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: { notEmpty: true },
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: { notEmpty: true },
+      },
+      avatar: { type: Sequelize.STRING },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('User');
+  },
+};
---
+++ migrations/20221206083300-create-news.js
@@ -0,0 +1,40 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('News', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: { notEmpty: true },
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: { notEmpty: true },
+      },
+      picture: { type: Sequelize.STRING },
+      tags: { type: Sequelize.STRING },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('News');
+  },
+};
