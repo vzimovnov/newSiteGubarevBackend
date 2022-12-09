ec8282ef052002cc70f4bbf42e3f7e560cd4c060
Pavel Gubarev <p.gubarev@nv.dunice.net>
2022-12-09 11:24:59 +0300

    fix migrations

--- migrations/20221206083101-create-user.js
+++ migrations/20221206083101-create-user.js
@@ -1,7 +1,6 @@
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('User', {
+    await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -42,6 +41,6 @@ module.exports = {
     });
   },
   async down(queryInterface) {
-    await queryInterface.dropTable('User');
+    await queryInterface.dropTable('Users');
   },
 };
